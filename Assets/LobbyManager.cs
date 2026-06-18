33647fa32c3f5f514223c97bfd898dac9b710607
Lucky1403 <lucky.230062289@gmail.com>
2026-06-18 21:57:08 +0530

    UI working with the Photon Connection

--- Assets/LobbyManager.cs
+++ Assets/LobbyManager.cs
@@ -8,17 +8,35 @@ public class LobbyManager : MonoBehaviourPunCallbacks
 {
     [Header("Login UI")]
     public InputField playerNameInputField;
+    public GameObject UI_loginGameObject;
+
+    [Header("Lobby UI")]
+    public GameObject UI_lobbyGameObject;
+    public GameObject UI_3dGameObject;
+
+    [Header("Connection Status UI")]
+    public GameObject UI_ConnectionStatusGameObject;
+    public Text ConnectionStatusText;
+    public bool showConnectionStatus = false;
     #region UNITY Methods
     // Start is called before the first frame update
     void Start()
     {
-        
+        UI_lobbyGameObject.SetActive(false);
+        UI_3dGameObject.SetActive(false);
+        UI_ConnectionStatusGameObject.SetActive(false);
+
+        UI_loginGameObject.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (showConnectionStatus)
+        {
+            ConnectionStatusText.text = "Connection Status: " + PhotonNetwork.NetworkClientState;
+
+        }
     }
     #endregion
 
@@ -28,12 +46,18 @@ public class LobbyManager : MonoBehaviourPunCallbacks
     {
         string playername = playerNameInputField.text;
 
-        if(! string.IsNullOrEmpty(playername))
+        if (!string.IsNullOrEmpty(playername))
         {
-            if(!PhotonNetwork.IsConnected)
+            UI_lobbyGameObject.SetActive(false);
+            UI_3dGameObject.SetActive(false);
+            UI_loginGameObject.SetActive(false);
+
+            showConnectionStatus = false; //-> Isko true kardenge toh UI mai connection status dikhne lagega abhi off kar rakha hai kyunki font sahi nahi lag raha hai. Chahe to isko true karke status ko UI mai dikha sakte hai
+            UI_ConnectionStatusGameObject.SetActive(true);
+            if (!PhotonNetwork.IsConnected)
             {
                 PhotonNetwork.LocalPlayer.NickName = playername;
-                
+
                 PhotonNetwork.ConnectUsingSettings();
             }
         }
@@ -53,7 +77,12 @@ public class LobbyManager : MonoBehaviourPunCallbacks
 
     public override void OnConnectedToMaster()
     {
+        UI_ConnectionStatusGameObject.SetActive(false);
+        UI_loginGameObject.SetActive(false);
+
+        UI_3dGameObject.SetActive(true);
+        UI_lobbyGameObject.SetActive(true);
         Debug.Log(PhotonNetwork.LocalPlayer.NickName + " is connected to Photon Server.");
-    } 
+    }
     #endregion
 }
